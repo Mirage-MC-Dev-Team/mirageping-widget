8d4db5494cc0b1976f7c7c922848cf3640218720
Carbolemons <34587311+Carbolemons@users.noreply.github.com>
2021-03-23 00:15:26 -0500

    colors.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $.getJSON(url, function (r) {
     return false;
   }
   var pl = "";
-  var motd = r.description.replaceColorCodes();
+  var motd = r.description.replace("").replaceColorCodes();
   for (i = 0; 5 > i; i++) {
     pl = pl + "<br>" + r.players.sample[i].name;
   }
